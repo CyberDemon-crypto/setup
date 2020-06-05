708431bf64d17003b3bd24a949f1327dd6db0004
Cyber Demon <XCobalt88@protonmail.com>
2020-06-05 09:32:53 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # made by https://github.com/CyberDemon-crypto
 sys=$(lsb_release -si)
-
 case $sys in
   ManjaroLinux|ArchLinux)
     pamac install git
@@ -10,29 +9,30 @@ case $sys in
     apt-get install git
     ;;
 esac
-
 pre='https://github.com/CyberDemon-crypto/'
-repos=($pre'Vowels_Consonants' $pre'P2P' $pre'matrix')
+repos=('vowels_consonants' 'P2P' 'matrix')
 clear
 echo [1]Install
 echo [2]Delete
 read option
 cd $HOME
-
+clear
 case $option in
   1)
     mkdir cyberdemon
     cd cyberdemon
     for repo in ${repos[*]}
       do
-      git clone "$repo"
+        echo "cloning into $repo..."
+        git clone "$pre$repo" -q
+        cd $repo
+        chmod +x "$repo.py"
+        cd ..
       done
-    clear
     echo '(ﾉ◕ヮ◕)ﾉ*:･ﾟ✧ my projects in:'
     pwd
     ;;
   2)
-    clear
     rm -r -f cyberdemon
     echo '(；⌣̀_⌣́) Deleted...'
     ;;
